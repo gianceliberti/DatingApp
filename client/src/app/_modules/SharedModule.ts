57aef599f69ad2c34a5a9dcb55c42885deff499f
gianceliberti <gianceliberti@gmail.com>
2021-08-22 15:44:17 -0300

    End of section 16

--- client/src/app/_modules/SharedModule.ts
+++ client/src/app/_modules/SharedModule.ts
@@ -9,6 +9,8 @@ import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { TimeagoModule } from 'ngx-timeago';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 
 @NgModule({
   declarations: [],
@@ -26,6 +28,7 @@ import { TimeagoModule } from 'ngx-timeago';
     PaginationModule.forRoot(),
     ButtonsModule.forRoot(),
     TimeagoModule.forRoot(),
+    ModalModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -37,7 +40,8 @@ import { TimeagoModule } from 'ngx-timeago';
     CommonModule,
     PaginationModule,
     ButtonsModule,
-    TimeagoModule
+    TimeagoModule,
+    ModalModule,
   ]
 })
 export class SharedModule {
