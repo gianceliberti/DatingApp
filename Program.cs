95a3ca1ad44deeb126388de0a201085c10e0982c
gianceliberti <gianceliberti@gmail.com>
2021-10-02 15:21:59 -0300

    heroku changes

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace API
                 var context = services.GetRequiredService<DataContext>();
                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-                await context.Database.MigrateAsync();
+                context.Database.Migrate();
                 await Seed.SeedUsers(userManager, roleManager);
             }
             catch (Exception ex)
